0e73b1608ca132ae6434cdf7f8f852ebe0975950
Xaliphostes <fmaerten@gmail.com>
2024-02-20 09:04:08 +0100

    added menu

--- src/lib/menu/MenuBuilder.ts
+++ src/lib/menu/MenuBuilder.ts
@@ -23,19 +23,42 @@ menu.append(
  *```
 */
 
-
+/*
+<nav class="navbar navbar-expand-lg navbar-light bg-light">
+  <a class="navbar-brand" href="#">Navbar</a>
+  <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNavAltMarkup" aria-controls="navbarNavAltMarkup" aria-expanded="false" aria-label="Toggle navigation">
+    <span class="navbar-toggler-icon"></span>
+  </button>
+  <div class="collapse navbar-collapse" id="navbarNavAltMarkup">
+    <div class="navbar-nav">
+      <a class="nav-item nav-link active" href="#">Home <span class="sr-only">(current)</span></a>
+      <a class="nav-item nav-link" href="#">Features</a>
+      <a class="nav-item nav-link" href="#">Pricing</a>
+      <a class="nav-item nav-link disabled" href="#">Disabled</a>
+    </div>
+  </div>
+</nav>
+*/
 export class MenuBuilder {
     private div: HTMLDivElement = undefined
     private ul: HTMLUListElement = undefined
 
     constructor(parentId: string, id: string = 'navbarSupportedContent') {
-        this.div = this.create('div', 'collapse navbar-collapse', id) as HTMLDivElement
-
-        this.ul = this.create('ul', 'navbar-nav me-auto mb-2 mb-lg-0') as HTMLUListElement
-        this.div.appendChild(this.ul)
-
+        this.div = this.create('div', 'navbar navbar-expand-lg navbar-light bg-light', id) as HTMLDivElement
+        this.div.innerHTML = `
+            <a class="navbar-brand" href="#">Arch-app</a>
+            <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNavAltMarkup" aria-controls="navbarNavAltMarkup" aria-expanded="false" aria-label="Toggle navigation">
+                <span class="navbar-toggler-icon"></span>
+            </button>
+            <div class="collapse navbar-collapse" id="navbarNavAltMarkup">
+                <ul class="navbar-nav" id="navbarNavAltMarkupUl"></ul>
+            </div>`
         const parent = document.getElementById(parentId)
         parent.append(this.div)
+
+        // this.ul = this.create('ul', 'navbar-nav me-auto mb-2 mb-lg-0') as HTMLUListElement
+        // this.div.appendChild(this.ul)
+        this.ul = document.getElementById('navbarNavAltMarkupUl') as HTMLUListElement
     }
 
     append(li: string) {
